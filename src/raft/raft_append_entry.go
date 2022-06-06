0e53182553eff2ec886db3a56542e7646e705884
megumigachi <472014574@qq.com>
2022-06-06 09:30:29 -0700

    passmost2c

--- src/raft/raft_append_entry.go
+++ src/raft/raft_append_entry.go
@@ -26,7 +26,8 @@ type AppendEntriesArgs struct {
 type AppendEntriesReply struct {
 	Term int
 	Success bool
-	NextIndex int
+	ConflictTerm int
+	ConflictIndex int
 }
 
 //when appending logs to server idx , get them
@@ -49,6 +50,7 @@ func (rf *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply
 
 	reply.Success=false
 	reply.Term=rf.currentTerm
+	reply.ConflictTerm=-1
 
 	argTerm:=args.Term
 	//condition 1(in figure 2 appendEntries)
@@ -65,29 +67,26 @@ func (rf *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply
 	prevLogidx:=args.PrevLogIndex
 	if prevLogidx>= len(rf.log) {
 		log.Printf("dealing append entries fail1 server id:%d, log start:%d,log len:%d,success:%v,rf log len:%d,time:%v\n",rf.me,args.PrevLogIndex,len(args.Entries),reply.Success, len(rf.log),time.Now().Sub(rf.startTime))
-		reply.NextIndex=len(rf.log)
+		reply.ConflictIndex=len(rf.log)
+
 		//reply.NextIndex=prevLogidx
 		return
 	}
 	if prevLogidx!=0&&rf.log[prevLogidx].Term!=args.PrevLogTerm {
 		log.Printf("dealing append entries fail2 server id:%d, log start:%d,log len:%d,success:%v,time:%v\n",rf.me,args.PrevLogIndex,len(args.Entries),reply.Success,time.Now().Sub(rf.startTime))
-		if rf.log[prevLogidx].Term>args.PrevLogIndex {
-			//skip a term
-			for i:=prevLogidx;i>=0 ;i--  {
-				if rf.log[i].Term<rf.log[prevLogidx].Term {
-					reply.NextIndex=i+1
-					break
-				}
+		//skip a term
+		for i:=prevLogidx;i>=0 ;i--  {
+			if rf.log[i].Term!=rf.log[prevLogidx].Term {
+				reply.ConflictIndex=i+1
+				break
 			}
-		}else {
-			reply.NextIndex=prevLogidx
 		}
-		reply.NextIndex=prevLogidx
+		reply.ConflictTerm=rf.log[prevLogidx].Term
 		return
 	}
 
 	reply.Success=true
-	reply.NextIndex=-1
+	reply.ConflictIndex=-1
 
 	if rf.role==Follower {
 		rf.electionTimer.Reset(ElectionTimeout*time.Millisecond)
@@ -178,7 +177,22 @@ func (rf* Raft) appendEntriesToFollower(idx int)  bool{
 							//target server refuse logs, reduce log idx
 							log.Printf("target server refuse logs,target id:%d,reply term:%d,\n",idx,reply.Term)
 							//rf.nextIndex[idx]--;
-							rf.nextIndex[idx]=reply.NextIndex
+							conTerm:=reply.ConflictTerm
+							if conTerm==-1 {
+								rf.nextIndex[idx]=reply.ConflictIndex
+							}else {
+								find:=false
+								for i:=args.PrevLogIndex;i>=0 ;i--  {
+									if rf.log[i].Term==conTerm {
+										rf.nextIndex[idx]=i+1
+										find=true
+										break
+									}
+								}
+								if !find {
+									rf.nextIndex[idx]=reply.ConflictIndex
+								}
+							}
 							rf.appendEntriesTimers[idx].Reset(0)
 						}
 					}else {
